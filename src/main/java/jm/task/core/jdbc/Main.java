758c630c01f0f67160422fac27dea2b33d17e064
Roman <safonov.roman.86@yandex.ru>
2022-06-30 18:27:16 +1000

    Используется UserService

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,19 +2,20 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl sqlDAO = new UserDaoJDBCImpl();
-        sqlDAO.createUsersTable();
-        sqlDAO.saveUser("Роман", "Сафонов", (byte) 36);
-        sqlDAO.saveUser("Виктор", "Александров", (byte) 20);
-        sqlDAO.saveUser("Сергей", "Борзой", (byte) 36);
-        sqlDAO.saveUser("Иван", "Дурак", (byte) 120);
-        for (User user : sqlDAO.getAllUsers()) {
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Роман", "Сафонов", (byte) 36);
+        userService.saveUser("Виктор", "Александров", (byte) 20);
+        userService.saveUser("Сергей", "Борзой", (byte) 36);
+        userService.saveUser("Иван", "Дурак", (byte) 120);
+        for (User user : userService.getAllUsers()) {
             System.out.println(user);
         }
-        sqlDAO.cleanUsersTable();
-        sqlDAO.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
